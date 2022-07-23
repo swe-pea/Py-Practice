710573c34dcfbf82c90aa56ef14d65d28344ae65
Ivan Camacho <Ivancamacho2578@gmail.com>
2022-07-23 10:32:46 -0700

    Fixed test_ex47.py to pass using the standard Python test lib

--- LearnPythonTheHardWay/rand/ex47/tests/test_ex47.py
+++ LearnPythonTheHardWay/rand/ex47/tests/test_ex47.py
@@ -1,12 +1,10 @@
-from typing_extensions import assert_never
-from nose.tools import *
 from ex47.game import Room
 
 def test_room():
     gold = Room("GoldRoom",
     """This room has gold in it you can grab. There's a door to the north.""")
-    assert_equal(gold.name, "GoldRoom")
-    assert_equal(gold.paths, {})
+    assert gold.name == "GoldRoom"
+    assert gold.paths == {}
 
 def test_room_paths():
     center = Room("Center", "Test room in the center.")
